216c64bc6b325b86fc1ea3213ef5c34c66f134ac
RizvanG <gasimov.fortech@gmail.com>
2023-06-23 17:33:13 +0300

    завернул в провайдеры

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { WeekDataProvider } from './context/WeekDataContext'
+import { ShowModalProvider } from './context/ShowModalContext'
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <WeekDataProvider>
+        <ShowModalProvider>
+          <App />
+        </ShowModalProvider>
+      </WeekDataProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
