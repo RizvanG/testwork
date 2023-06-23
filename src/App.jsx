1763841ad9c100cb0d15b253e5177cc98b6508ef
RizvanG <gasimov.fortech@gmail.com>
2023-06-23 17:32:13 +0300

    контекст для модального окна

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import Main from "./components/Main/Main";
 import Sidebar from "./components/Sidebar/Sidebar";
 import UsersList from "./components/UsersList/UsersList";
+import { useContext } from "react";
+import { ShowModalContext } from "./context/ShowModalContext";
+import Modal from "./components/Modal/Modal";
 
 function App() {
+  const { showModal } = useContext(ShowModalContext);
+
   return (
     <div className="wrapper">
       <Sidebar />
+      {showModal && <Modal />}
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/dashboard" element={<Dashboard />} />
